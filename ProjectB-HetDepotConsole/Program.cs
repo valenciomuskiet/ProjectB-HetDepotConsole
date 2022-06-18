e5e42b8a06b888052199e23970bfe0d9981719ff
valenciomuskiet <94371531+valenciomuskiet@users.noreply.github.com>
2022-06-18 23:44:02 +0200

    Add files via upload
    hello world !

---
+++ ProjectB-HetDepotConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectB_HetDepotConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
